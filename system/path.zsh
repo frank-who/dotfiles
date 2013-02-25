fa77b2d75dde4d3bc3bae0032b005073764ddb88
Frank <me@frankhq.io>
2013-02-25 22:17:34 +0200

    Added heroku

--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,7 @@
 # ./bin - binstubs
 # /usr/local/bin - homebrew
+# /usr/local/heroku/bin - heroku
 # $HOME/.rbenv/bin - rbenv
 # $HOME/.rbenv/shims - pow
 
-export PATH="./bin:/usr/local/bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/heroku/bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
