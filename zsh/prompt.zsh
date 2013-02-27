43bfb4b2500bc3e4b9aa44675e92fc4044952ac4
Frank <me@frankhq.io>
2013-02-27 09:37:16 +0200

    Prompt tweaks

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,16 +5,16 @@ else
 fi
 
 git_branch() {
-  RQUERY=$($git symbolic-ref HEAD 2> /dev/null) || return
+  RQUERY=$(git symbolic-ref HEAD 2> /dev/null) || return
   SQUERY=$(git status 2> /dev/null | tail -n 1)
   BRANCH="${RQUERY#refs/heads/}"
   if [[ $SQUERY == '' ]]; then
     echo
   else
     if [[ $SQUERY =~ ^nothing ]]; then
-      echo "on %{$fg_bold[green]%}${BRANCH}%{$reset_color%}" # Clean
+      echo " on %{$fg_bold[green]%}${BRANCH}%{$reset_color%}" # Clean
     else
-      echo "on %{$fg_bold[red]%}${BRANCH}%{$reset_color%}" # Eeeew
+      echo " on %{$fg_bold[red]%}${BRANCH}%{$reset_color%}" # Eeeew
     fi
   fi
 }
@@ -24,27 +24,24 @@ git_status() {
   MARK=''
 
   if $(echo "$QUERY" | grep '^?? ' &> /dev/null); then
-    MARK="%{$fg_bold[green]%}?%{$reset_color%}" # Untracked
+    MARK="%{$fg_bold[green]%}•%{$reset_color%}" # Untracked
   fi
 
   echo $MARK
 }
 
-unpushed() {
-  $git cherry -v @{upstream} 2> /dev/null
-}
-
-need_push() {
-  if [[ $(unpushed) == '' ]]; then
-    echo ''
+git_need_push() {
+  QUERY=$(git cherry -v @{upstream} 2> /dev/null)
+  if [[ $QUERY == '' ]]; then
+    echo ' '
   else
-    echo "%{$fg_bold[magenta]%}!%{$reset_color%}"
+    echo "%{$fg_bold[magenta]%}•%{$reset_color%} "
   fi
 }
 
 ruby_version() {
   if (( $+commands[rbenv] )); then
-    echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
+    echo "%{$fg[yellow]%}‹$(rbenv version | awk '{print $1}')›%{$reset_color%} "
   else
     echo ''
   fi
@@ -62,8 +59,8 @@ host_name() {
  echo "%{$fg_bold[yellow]%}%m%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(directory_name) $(git_branch)$(git_status)$(need_push)\n$ '
-export RPROMPT=$'$(ruby_version)'
+export PROMPT=$'\n$(directory_name)$(git_branch)$(git_status)$(git_need_push)$(ruby_version)\n$ '
+#export RPROMPT='$(ruby_version)'
 
 precmd() {
   #title "zsh" "%m" "%55<...<%~"
