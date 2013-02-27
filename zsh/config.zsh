002081e84c04566f244afe83d154b1926f3cd1a4
Frank <me@frankhq.io>
2013-02-27 09:36:16 +0200

    Do not share history

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -21,7 +21,7 @@ setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
+unsetopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 unsetopt CORRECT
