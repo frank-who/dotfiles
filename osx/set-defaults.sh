9f86ef5fb02c5de7977f26e893bf92c51e2996d1
franks921 <frank@6ix.io>
2016-12-18 22:39:23 +0200

    changes

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,12 +1,11 @@
-# Sketch http://bohemiancoding.com/sketch/support/documentation/13-preferences/
-defaults write com.bohemiancoding.sketch3 exportCompactSVG -bool yes
-
-
 # No autohide delay on Dock
 defaults write com.apple.Dock autohide-delay -bool no
 
-# Make Dock in Mavericks transparent
+# Make Dock in Mavericks + transparent
 defaults write com.apple.dock hide-mirror -bool yes
 
 # Don't save to iCloud by default
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool no
+
+# Show hidden files
+defaults write com.apple.finder AppleShowAllFiles -bool yes
--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -2,6 +2,4 @@ bindkey -e
 
 bindkey '^[[H'  beginning-of-line # [fn][<]
 bindkey '^[[F'  end-of-line       # [fn][>]
-
 bindkey '^[[3~' delete-char       # [fn][<--]: Forward deleting
-
