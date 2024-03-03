205e41a54de584a6c2af62b93a1cb99ad54cf471
Frank <frank@6ix.io>
2024-03-03 19:55:28 +0200

    Commit: 2024-03-03--19:55

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -2,7 +2,7 @@ export ZPLUG_HOME=~/.zplug
 source $ZPLUG_HOME/init.zsh
 
 zplug "zdharma/fast-syntax-highlighting", as:plugin, defer:2
-zplug "zsh-users/zsh-autosuggestions", as:plugin, defer:2
+# zplug "zsh-users/zsh-autosuggestions", as:plugin, defer:2
 zplug load
 
 if ! zplug check --verbose; then
